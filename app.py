a96a8cf90b61fe770ece66aab55a603d6b1b5b6c
sirivela01 <ysirivelabtech23@ced.alliance.edu.in>
2026-08-31 18:37:36 +0530

    Disable browser caching in Flask backend to force client update

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ load_dotenv()
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
+@app.after_request
+def add_header(response):
+    # Disable browser caching for all responses (especially index.html)
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
+
 @app.route('/')
 def index():
     return render_template('index.html')
